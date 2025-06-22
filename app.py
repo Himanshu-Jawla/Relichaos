6132ac1ba80f87ae3f160edf41f001ce2f2a862e
Himanshu Jawla <himanshujawla230@gmail.com>
2025-06-22 19:57:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
-
 from flask import Flask, render_template, request, redirect
-import csv
-import os
 
-app = Flask(__name__, template_folder='../html_templates', static_folder='../static_assets')
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/preorder', methods=['POST'])
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+@app.route("/preorder", methods=["GET", "POST"])
 def preorder():
-    name = request.form['name']
-    email = request.form['email']
-    cap = request.form['cap']
+    if request.method == "POST":
+        
+        return redirect("/success")
+    return render_template("preorder.html")
 
-    data_path = os.path.join(os.path.dirname(__file__), 'orders.csv')
-    with open(data_path, 'a', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow([name, email, cap])
-
-    return redirect('/')
-
-if __name__ == '__main__':
+@app.route("/success")
+def success():
+    return render_template("success.html")
+    
+if __name__ == "__main__":
     app.run(debug=True)
